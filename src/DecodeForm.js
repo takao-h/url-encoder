3f50b504266ed411145358fa61290233942ce4cb
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-02-17 22:02:29 +0900

    insert

--- src/DecodeForm.js
+++ src/DecodeForm.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class Decode extends React.Component {
-  
+
   render(){
     return(
       <Form>
         <FormGroup row>
-        <Label for="URL" sm={2}>URL</Label>
-        <Col sm={10}>
+        <Label for="URL" md={2}>URL</Label>
+        <Col md={10}>
           <Input type="text" name="URL" id="URL" placeholder="URL" />
         </Col>
         </FormGroup>
         <FormGroup row>
-        <Label for="encodedURL" sm={2}>encoded URL</Label>
-        <Col sm={10}>
+        <Label for="encodedURL" md={2}>encoded URL</Label>
+        <Col md={10}>
           <Input type="text" name="encodedURL" id="encodedURL" placeholder="deencoded URL" />
         </Col>
         </FormGroup>
